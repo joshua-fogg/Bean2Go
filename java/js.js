a2ea1f7ba66cacd653f55a30b6bcca4d81caff78
Joshua Fogg <joshuafoggnz@gmail.com>
2017-06-21 16:32:09 +1200

    savepoint endofday

--- java/js.js
+++ java/js.js
@@ -1,16 +1,16 @@
-function openCity(evt, cityName) {
+function openCity(tabName,elmnt,color) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].style.backgroundColor = "";
     }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+    document.getElementById(tabName).style.display = "block";
+    elmnt.style.backgroundColor = color;
 
+}
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
